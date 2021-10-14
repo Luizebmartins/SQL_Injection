aab026fecd31090642676cb23d756406a20b3858
Luiz Martins <luizmartins.prog@gmail.com>
2021-10-14 18:56:10 -0300

    adicionada rota de login vulnerável

--- server/routes/productsRoute.js
+++ server/routes/productsRoute.js
@@ -2,11 +2,29 @@ const express = require('express')
 const database = require('../infra/database')
 const router = express.Router()
 
-router.get('/products/:id', async (req, res) => {
-    const id = req.params.id
-    const results = await database.query(`select * from produtos where id =${id}`)
-    res.json(results)
+router.get('/products/:nome', async (req, res) => {
+    try {
+        const results = await database.query(`select * from produtos where nome ='${req.params.nome}'`)
+        res.status(200).json(results)
+        
+    } catch (e) {
+        res.status(404).send(e.message)
+    }
 
+})
+
+router.post('/login', async (req, res) => {
+    const userData = req.body
+    try {
+        const user = await database.query(`select * from usuarios where login = '${userData.nome}' and senha = '${userData.senha}'`)
+        if(!user.length) throw new Error ('Dados incorretos')
+
+        res.status(200).json(user)
+        
+    } catch (e) {
+        res.status(401).send(e.message)
+    }
+    
 })
 
 module.exports = router
